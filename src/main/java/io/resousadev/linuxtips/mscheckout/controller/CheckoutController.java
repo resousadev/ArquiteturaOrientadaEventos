a2a69fddadbfa3f4a4f6835c6c60b2f9e5f6b3c2
renanrcs <renan.cavalcante@yahoo.com>
2025-11-20 13:38:35 -0300

    Add CheckoutController for handling order completion requests

---
+++ src/main/java/io/resousadev/linuxtips/mscheckout/controller/CheckoutController.java
@@ -0,0 +1,24 @@
+package io.resousadev.linuxtips.mscheckout.controller;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import io.resousadev.linuxtips.mscheckout.model.Payment;
+import io.resousadev.linuxtips.mscheckout.producer.EventBridgeProducer;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("v1/mscheckout")
+public class CheckoutController {
+
+    private final EventBridgeProducer eventBridgeProducer;
+
+    @PostMapping("/orders")
+    public void finishOrder(@RequestBody Payment payment) {
+        eventBridgeProducer.finishOrder(payment);
+    }
+
+}
