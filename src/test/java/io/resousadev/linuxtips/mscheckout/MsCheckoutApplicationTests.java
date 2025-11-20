f68d74b268ed85151e20a79ecc56d43e879e76ec
resousadev <resousadev@gmail.com>
2025-11-20 12:39:49 -0300

    Add configuration to exclude commons-logging and rename package of test

--- src/test/java/io/resousadev/linuxtips/mscheckout/MsCheckoutApplicationTests.java
+++ src/test/java/io/resousadev/linuxtips/mscheckout/MsCheckoutApplicationTests.java
@@ -1,4 +1,4 @@
-package io.resousadev.linuxtips.ms_checkout;
+package io.resousadev.linuxtips.mscheckout;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
