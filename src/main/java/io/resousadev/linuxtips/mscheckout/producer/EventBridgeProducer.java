6a931dbc56d40f63ce22ef83a45b88e814764020
renanrcs <renan.cavalcante@yahoo.com>
2025-11-20 13:38:18 -0300

    Fix method calls in EventBridgeProducer for payment status and value retrieval

--- src/main/java/io/resousadev/linuxtips/mscheckout/producer/EventBridgeProducer.java
+++ src/main/java/io/resousadev/linuxtips/mscheckout/producer/EventBridgeProducer.java
@@ -3,8 +3,8 @@ package io.resousadev.linuxtips.mscheckout.producer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 
+import io.resousadev.linuxtips.mscheckout.model.Payment;
 import lombok.extern.slf4j.Slf4j;
-import software.amazon.awssdk.auth.credentials.AwsCredentialsProvider;
 import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.eventbridge.EventBridgeClient;
@@ -27,8 +27,8 @@ public class EventBridgeProducer {
         log.info("Enviando evento para o Event Bridge...");
         PutEventsRequestEntry eventRequest = PutEventsRequestEntry.builder()
             .source("ms-checkout")
-            .detailType(payment.status)
-            .detail("{ \"valor\": \"" + payment.valor + "\" }")
+            .detailType(payment.status())
+            .detail("{ \"valor\": \"" + payment.valor() + "\" }")
             .eventBusName("status-pedido-bus")
             .build();
 
