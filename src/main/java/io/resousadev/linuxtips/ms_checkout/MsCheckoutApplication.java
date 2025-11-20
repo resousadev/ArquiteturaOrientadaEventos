091afe25f5d94e72412cf9db4d5d2b253ac042d9
renanrcs <renan.cavalcante@yahoo.com>
2025-11-20 11:05:09 -0300

    Initial Commit

---
+++ src/main/java/io/resousadev/linuxtips/ms_checkout/MsCheckoutApplication.java
@@ -0,0 +1,13 @@
+package io.resousadev.linuxtips.ms_checkout;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsCheckoutApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsCheckoutApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/resousadev/linuxtips/ms_checkout/MsCheckoutApplicationTests.java
@@ -0,0 +1,13 @@
+package io.resousadev.linuxtips.ms_checkout;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsCheckoutApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
