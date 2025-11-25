303f9cbe5bc98ea152602117cd460898778d1719
renanrcs <renan.cavalcante@yahoo.com>
2025-11-24 22:05:49 -0300

    Add UsuarioService for user management and password encryption

---
+++ src/main/java/io/resousadev/linuxtips/mscheckout/service/UsuarioService.java
@@ -0,0 +1,28 @@
+package io.resousadev.linuxtips.mscheckout.service;
+
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import io.resousadev.linuxtips.mscheckout.model.Usuario;
+import io.resousadev.linuxtips.mscheckout.repository.UsuarioRepository;
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class UsuarioService {
+
+    private final UsuarioRepository usuarioRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    public void salvarUsuario(Usuario usuario) {
+        // Criptografa a senha antes de salvar
+        String senhaCriptografada = passwordEncoder.encode(usuario.getSenha());
+        usuario.setSenha(senhaCriptografada);
+        usuarioRepository.save(usuario);
+    }
+
+    public Usuario buscarPorLogin(String login) {
+        return usuarioRepository.findByLogin(login);
+    }
+
+}
